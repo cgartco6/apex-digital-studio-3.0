9b41f4e31017af18b31b4af37fdb1ab87e2a652a
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-02-05 09:41:48 +0200

    Update dependencies.py

--- backend/app/auth/dependencies.py
+++ backend/app/auth/dependencies.py
@@ -18,3 +18,6 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
 
     db = SessionLocal()
     user = db.query(User).get(user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
